ad42e944642b5e8f14e900d18dd3f7c9bd388a37
Frank Force <frankforce@gmail.com>
2021-09-25 17:53:58 -0500

    fix puzzle tiles

--- examples/puzzle/game.js
+++ examples/puzzle/game.js
@@ -186,7 +186,7 @@ function gameRenderPost()
 
 ///////////////////////////////////////////////////////////////////////////////
 // Startup LittleJS Engine
-engineInit(gameInit, gameUpdate, gameUpdatePost, gameRender, gameRenderPost, 'tiles.png');
+engineInit(gameInit, gameUpdate, gameUpdatePost, gameRender, gameRenderPost, 'tiles.png?1');
 
 ///////////////////////////////////////////////////////////////////////////////
 function clearMatches()
