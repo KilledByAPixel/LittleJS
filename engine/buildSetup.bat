9fa32f958658557f3b96810d59207cdfd7729d3f
Frank Force <frankforce@gmail.com>
2021-09-30 23:28:14 -0500

    fix uglify install

--- engine/buildSetup.bat
+++ engine/buildSetup.bat
@@ -1,6 +1,6 @@
 rem Install these tools to build LittleJS
 
 call npm install -g google-closure-compiler
-call npm install -g uglifyjs
+call npm install -g uglify-js
 call npm install -g roadroller
 call npm install -g ect-bin
