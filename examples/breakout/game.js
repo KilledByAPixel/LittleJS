551a551c2cf7bd8db86f0920daf2270989eb6250
Frank Force <frankforce@gmail.com>
2021-11-22 12:33:47 -0600

    Update game.js

--- examples/breakout/game.js
+++ examples/breakout/game.js
@@ -66,4 +66,4 @@ function gameRenderPost()
 
 ///////////////////////////////////////////////////////////////////////////////
 // Startup LittleJS Engine
-engineInit(gameInit, gameUpdate, gameUpdatePost, gameRender, gameRenderPost, 'tiles.png');
+engineInit(gameInit, gameUpdate, gameUpdatePost, gameRender, gameRenderPost, 'tiles.png?1');
