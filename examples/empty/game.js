138fca2fd64057499b796f817d490d3efc4893d3
Frank Force <frankforce@gmail.com>
2023-07-12 13:05:33 -0500

    Update game.js

--- examples/empty/game.js
+++ examples/empty/game.js
@@ -7,26 +7,36 @@
 ///////////////////////////////////////////////////////////////////////////////
 function gameInit()
 {
+    // called once after the engine starts up
+    // setup the game
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 function gameUpdate()
 {
+    // called every frame at 60 frames per second
+    // handle input and update the game state
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 function gameUpdatePost()
 {
+    // called after physics and objects are updated
+    // setup camera and prepare for render
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 function gameRender()
 {
+    // called before objects are rendered
+    // draw any background effects that appear behind objects
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 function gameRenderPost()
 {
+    // called after objects are rendered
+    // draw effects or hud that appear above all objects
     drawTextScreen('Hello World!', mainCanvasSize.scale(.5), 80);
 }
 
