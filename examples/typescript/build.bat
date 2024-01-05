f3e207d072d7b908537adbdb016059d1d4431e70
Frank Force <frankforce@gmail.com>
2024-01-05 15:15:09 -0600

    use node for typescript demo build

--- examples/typescript/build.bat
+++ examples/typescript/build.bat
@@ -1,14 +1,2 @@
-rem TypeScript build script for LittleJS by Frank Force
-
-set BUILD_FOLDER=build
-
-call npx tsc
-if %ERRORLEVEL% NEQ 0 (
-    pause
-    exit /b %ERRORLEVEL%
-)
-
-rem copy output js file and remove build folder
-
-copy %BUILD_FOLDER%\examples\typescript\game.js game.js
-rmdir /s /q %BUILD_FOLDER%
+rem LittleJS Build Script
+call node build.js
