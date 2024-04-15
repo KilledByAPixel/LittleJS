e97ff5032edd38f05c2dfc4e286414de46d626c4
Frank Force <frankforce@gmail.com>
2024-04-14 22:26:35 -0500

    webgl2!
    only a few changes to hook this up, everything is pretty much the same
    - added #version 300 es
    - changed attribute to in and varying to out

--- src/engineWebGL.js
+++ src/engineWebGL.js
@@ -31,29 +31,32 @@ function glInit()
 {
     // create the canvas and textures
     glCanvas = document.createElement('canvas');
-    glContext = glCanvas.getContext('webgl', {antialias: !canvasPixelated});
+    glContext = glCanvas.getContext('webgl2');
 
     // some browsers are much faster without copying the gl buffer so we just overlay it instead
     glOverlay && document.body.appendChild(glCanvas);
 
     // setup vertex and fragment shaders
     glShader = glCreateProgram(
+        '#version 300 es\n' +         // specify GLSL ES version
         'precision highp float;'+     // use highp for better accuracy
         'uniform mat4 m;'+            // transform matrix
-        'attribute vec2 p,t;'+        // position, uv
-        'attribute vec4 c,a;'+        // color, additiveColor
-        'varying vec4 v,d,e;'+        // return uv, color, additiveColor
+        'in vec2 p,t;'+               // position, uv
+        'in vec4 c,a;'+               // color, additiveColor
+        'out vec4 v,d,e;'+            // return uv, color, additiveColor
         'void main(){'+               // shader entry point
         'gl_Position=m*vec4(p,1,1);'+ // transform position
         'v=vec4(t,p);d=c;e=a;'+       // pass stuff to fragment shader
         '}'                           // end of shader
         ,
-        'precision highp float;'+              // use highp for better accuracy
-        'varying vec4 v,d,e;'+                 // uv, color, additiveColor
-        'uniform sampler2D s;'+                // texture
-        'void main(){'+                        // shader entry point
-        'gl_FragColor=texture2D(s,v.xy)*d+e;'+ // modulate texture by color plus additive
-        '}'                                    // end of shader
+        '#version 300 es\n' +         // specify GLSL ES version
+        'precision highp float;'+     // use highp for better accuracy
+        'in vec4 v,d,e;'+             // uv, color, additiveColor
+        'uniform sampler2D s;'+       // texture
+        'out vec4 c;'+                // out color
+        'void main(){'+               // shader entry point
+        'c=texture(s,v.xy)*d+e;'+     // modulate texture by color plus additive
+        '}'                           // end of shader
     );
 
     // init buffers
@@ -304,25 +307,28 @@ function glInitPostProcess(shaderCode, includeOverlay)
 {
     ASSERT(!glPostShader); // can only have 1 post effects shader
 
-    if (!shaderCode) // default shader
-        shaderCode = 'void mainImage(out vec4 c,vec2 p){c=texture2D(iChannel0,p/iResolution.xy);}';
+    if (!shaderCode) // default shader pass through
+        shaderCode = 'void mainImage(out vec4 c,vec2 p){c=texture(iChannel0,p/iResolution.xy);}';
 
     // create the shader
     glPostShader = glCreateProgram(
+        '#version 300 es\n' +            // specify GLSL ES version
         'precision highp float;'+        // use highp for better accuracy
-        'attribute vec2 p;'+             // position
+        'in vec2 p;'+                    // position
         'void main(){'+                  // shader entry point
         'gl_Position=vec4(p,1,1);'+      // set position
         '}'                              // end of shader
         ,
+        '#version 300 es\n' +            // specify GLSL ES version
         'precision highp float;'+        // use highp for better accuracy
         'uniform sampler2D iChannel0;'+  // input texture
         'uniform vec3 iResolution;'+     // size of output texture
         'uniform float iTime;'+          // time passed
+        'out vec4 c;'+                   // out color
         '\n' + shaderCode + '\n'+        // insert custom shader code
         'void main(){'+                  // shader entry point
-        'mainImage(gl_FragColor,gl_FragCoord.xy);'+ // call post process function
-        'gl_FragColor.a=1.;'+            // always use full alpha
+        'mainImage(c,gl_FragCoord.xy);'+ // call post process function
+        'c.a=1.;'+                       // always use full alpha
         '}'                              // end of shader
     );
 
