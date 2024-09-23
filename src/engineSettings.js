b2bce019deea3b59dc1832e1e476138912fb59c3
Frank Force <frankforce@gmail.com>
2024-09-23 11:01:21 -0500

    increased tileFixBleedScale to eliminated bleeding
    after testing some extreme cases with very tiny sprites and zoomed in frame buffer I found that to fully eliminate the black lines i had to set it to .4, and theorizing that still may not cover everything I set it to .5

--- src/engineSettings.js
+++ src/engineSettings.js
@@ -89,7 +89,7 @@ let tileSizeDefault = vec2(16);
  *  @type {Number}
  *  @default
  *  @memberof Settings */
-let tileFixBleedScale = .1;
+let tileFixBleedScale = .5;
 
 ///////////////////////////////////////////////////////////////////////////////
 // Object settings
