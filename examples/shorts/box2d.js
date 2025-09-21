465b07b84954d66089896c4fa099bbd626847a1c
Frank Force <frankforce@gmail.com>
2025-09-21 17:08:27 -0500

    added mini box2d demos

---
+++ examples/shorts/box2d.js
@@ -0,0 +1,47 @@
+async function gameInit()
+{
+    // setup box2d
+    await box2dInit();
+    mouseJoint = 0;
+    gravity.y = -50;
+    
+    // create ground object
+    groundObject = new Box2dObject(vec2(-8), vec2(), 0, 0, GRAY, box2d.bodyTypeStatic);
+    groundObject.addBox(vec2(100,2));
+
+    // add some random objects
+    for(let i=50; i--;)
+    {
+        const o = new Box2dObject(randInCircle(5), vec2(), 0, 0, randColor());
+        randInt(2) ? o.addCircle(rand(1,2)) : o.addRandomPoly(rand(1,2));
+    }
+}
+
+function gameUpdate()
+{
+    // mouse controls
+    if (mouseJoint)
+    {
+        // update mouse joint
+        mouseJoint.setTarget(mousePos);
+        if (mouseWasReleased(0))
+        {
+            // release object
+            mouseJoint.destroy();
+            mouseJoint = 0;
+        }
+    }
+    else if (mouseWasPressed(0))
+    {
+        // grab object
+        const object = box2d.pointCast(mousePos);
+        if (object)
+            mouseJoint = new Box2dTargetJoint(object, groundObject, mousePos);
+    }
+}
+
+function gameRenderPost()
+{
+    // draw mouse joint
+    mouseJoint && drawLine(mousePos, mouseJoint.getAnchorB(), .2, RED);
+}
---
+++ examples/shorts/box2dCar.js
@@ -0,0 +1,49 @@
+async function gameInit()
+{
+    // setup box2d and create the objects
+    await box2dInit();
+    gravity.y = -20;
+    groundObject = new Box2dObject(vec2(-8), vec2(), 0, 0, GRAY, box2d.bodyTypeStatic);
+    groundObject.addBox(vec2(100,6));
+    new CarObject(vec2(0,-2));
+}
+
+class CarObject extends Box2dObject
+{
+    constructor(pos)
+    {
+        super(pos, vec2(), 0, 0, RED);
+
+        // create a car with wheels
+        this.addBox(vec2(7,2));
+        const frequency = 4, maxTorque = 250;
+        this.wheels = [];
+        for(let i=2; i--;)
+        {
+            const wheelPos = pos.add(vec2(i?2:-2, -1));
+            const wheel = new Box2dObject(wheelPos, vec2(2), tile(0));
+            const joint = new Box2dWheelJoint(this, wheel);
+            joint.setSpringFrequencyHz(frequency);
+            joint.setMaxMotorTorque(maxTorque);
+            joint.enableMotor(!i);
+            wheel.addCircle(2);
+            wheel.motorJoint = joint;
+            this.wheels[i] = wheel;
+        }
+    }
+    update()
+    {
+        // car controls - use arrow keys or A/D to drive
+        const maxSpeed = 40, brakeAmount = .9;
+        const input = keyDirection().x;
+        let s = this.wheels[0].motorJoint.getMotorSpeed();
+        s = input ? clamp(s - input, -maxSpeed, maxSpeed) : s * brakeAmount;
+        this.wheels[0].motorJoint.setMotorSpeed(s);
+        super.update();
+    }
+    destroy()
+    {
+        this.wheels.forEach(o=>o.destroy());
+        super.destroy();
+    }
+}
