a3b52cdbea80ca4e313ca634b546a9f66504c004
Frank Force <frankforce@gmail.com>
2025-09-25 15:09:40 -0500

    raycasting demo

---
+++ examples/shorts/raycasting.js
@@ -0,0 +1,48 @@
+let playerPos = vec2(), playerAngle = 0;
+const levelTest = (pos)=> ((pos.x|0)**2|(pos.y|0)**4)%3>1;
+
+function raycast(pos, angle)
+{
+    const maxDistance=40, delta=.05;
+    const rayDir = vec2(0,delta).rotate(angle);
+    pos = pos.copy();
+    for (let distance = 0; distance<maxDistance; distance+=delta)
+    {
+        if (levelTest(pos))
+            return distance/maxDistance;
+        pos.x += rayDir.x;
+        pos.y += rayDir.y;
+    }
+    return 1;
+}
+
+function gameUpdate()
+{
+    // rotate player with arrow keys
+    const moveInput = keyDirection();
+    playerAngle += moveInput.x * .1;
+
+    // move forward/backward, prevent walking through walls
+    const newPlayerPos = playerPos.add(vec2(0,moveInput.y*.1).rotate(playerAngle));
+    if (!levelTest(newPlayerPos))
+        playerPos = newPlayerPos;
+}
+
+function gameRender()
+{
+    // draw floor and ceiling
+    const h=9;
+    for (let y=-h; y<h; y+=.1)
+    {
+        const p = 1.01-abs(y/h)
+        drawRect(vec2(0,y), vec2(39,.11), hsl(.1,y>0?0:.5,.7-p*.7));
+    }
+
+    // draw vertical slices to create the walls
+    const w=20;
+    for (let x=-w; x<w; x+=.1)
+    {
+        const p = raycast(playerPos, playerAngle + x/w);
+        drawRect(vec2(x,0), vec2(.11,.4/p), hsl(.6,.5,.7-p*.7));
+    }
+}
