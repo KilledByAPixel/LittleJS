b38d9a013464ad2e907d1b61a1c68d3e56f244ca
Frank Force <frankforce@gmail.com>
2025-09-26 18:15:03 -0500

    add medals example

---
+++ examples/shorts/medals.js
@@ -0,0 +1,42 @@
+// create example medals
+const medal_openedExample = new Medal(0, 'Opened Example',   'You opened this example!');
+const medal_leftClick     = new Medal(1, 'Left Clicked',     'You left clicked!',     '🐁');
+const medal_rightClick    = new Medal(2, 'Right Clicked',    'You right clicked!',    '🐭');
+const medal_spacePressed  = new Medal(3, 'Pressed Spacebar', 'You pressed spacebar!', '🚀');
+
+// setup medals
+const saveName = 'Medals Example';
+medalsInit(saveName);
+
+// clear medals for testing
+medalsForEach(medal=> medal.unlocked = false);
+medal_openedExample.unlock();
+
+function gameUpdate()
+{
+    if (mouseWasPressed(0))
+        medal_leftClick.unlock();
+    if (mouseWasPressed(2))
+        medal_rightClick.unlock();
+    if (keyWasPressed('Space'))
+        medal_spacePressed.unlock();
+}
+
+function gameRender()
+{
+    const size = 80;
+    let pos = mainCanvasSize.scale(.5).subtract(vec2(0,40));
+    drawTextScreen('Unlocked Medals', pos, size);
+
+    // show unlocked medals
+    let medalsCount = 0;
+    medalsForEach(medal=> medal.unlocked && medalsCount++);
+    pos = pos.add(vec2((1-medalsCount)*size/2, 100));
+    medalsForEach(medal=>
+    {
+        if (!medal.unlocked)
+            return;
+        medal.renderIcon(pos, size);
+        pos.x += size + 8;
+    });
+}
