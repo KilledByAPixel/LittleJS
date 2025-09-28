18a636cf6abd7e5c7ce4a321f585b4f0810441c0
Frank Force <frankforce@gmail.com>
2025-09-27 23:04:32 -0500

    Hello World

--- examples/shorts/helloWorld.js
+++ examples/shorts/helloWorld.js
@@ -1,8 +1,11 @@
 function gameRender()
 {
-    // draw text
-    drawText('Hello World', vec2(0,2), 3);
+    // draw background rect
+    drawRect(cameraPos, vec2(30), hsl(0,0,.2));
+
+    // draw text to the overlay canvas
+    drawTextOverlay('Hello World', vec2(0,3), 3);
         
     // draw a tile
-    drawTile(vec2(0,-2), vec2(7), tile(3,128));
+    drawTile(vec2(Math.sin(time)*3,-2), vec2(7), tile(3,128));
 }
