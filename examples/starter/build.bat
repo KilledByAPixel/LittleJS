224b96445e6a4853767f27b0db8d55547cf3cf5b
Frank Force <frankforce@gmail.com>
2025-09-28 01:46:26 -0500

    pause if build fails

--- examples/starter/build.bat
+++ examples/starter/build.bat
@@ -1,2 +1,7 @@
 rem LittleJS Build Script
 call node build.js
+if %errorlevel% neq 0 (
+    echo Build failed with error level %errorlevel%
+    pause
+    exit /b %errorlevel%
+)
--- src/engineBuild.bat
+++ src/engineBuild.bat
@@ -1,2 +1,7 @@
 rem LittleJS Build Script
 call npm run build
+if %errorlevel% neq 0 (
+    echo Build failed with error level %errorlevel%
+    pause
+    exit /b %errorlevel%
+)
