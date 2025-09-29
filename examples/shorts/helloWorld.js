64fd7f7d54c210bf15ea39fbc0b2ca0e625d939a
Frank Force <frankforce@gmail.com>
2025-09-29 14:06:22 -0500

    make background larger

--- examples/shorts/helloWorld.js
+++ examples/shorts/helloWorld.js
@@ -1,7 +1,7 @@
 function gameRender()
 {
     // draw background rect
-    drawRect(cameraPos, vec2(30), hsl(0,0,.2));
+    drawRect(cameraPos, vec2(50), hsl(0,0,.2));
 
     // draw text to the overlay canvas
     drawTextOverlay('Hello World', vec2(0,3), 3);
