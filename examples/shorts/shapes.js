1cf8c90c43c91b10186a2cc19ef665b850ebe6c8
Frank Force <frankforce@gmail.com>
2025-10-03 14:31:24 -0500

    added more shapes

--- examples/shorts/shapes.js
+++ examples/shorts/shapes.js
@@ -1,16 +1,18 @@
 function gameRender()
 {
-    // polygon shapes
-    for (let j=3; j<7; ++j)
-        drawRegularPoly(vec2(j*4-18,0), vec2(1), j, hsl(j/4+time/9,1,.5), .2, WHITE, time);
-
     // circles and ellipses
-    drawCircle(vec2(0,5), 1+wave(.5), hsl(.15,1,.5));
-    drawEllipse(vec2(-5,5), vec2(2,1), hsl(0,1,.5));
-    drawEllipse(vec2(5,5), vec2(.5,2), hsl(.55,1,.5), .5);
+    drawCircle(vec2(0,5), 1+wave(.5), RED);
+    drawEllipse(vec2(-6,5), vec2(1,1), YELLOW, .5);
+    drawEllipse(vec2(6,5), vec2(1,2), CLEAR_BLACK, .3, .5, CYAN)
+
+    // polygon shapes
+    drawRectGradient(vec2(-6,0), vec2(5,4), RED, BLUE)
+    drawRegularPoly(vec2(0,0), vec2(2), 3, PURPLE, .2, WHITE, time);
+    const starPath = [vec2(0,2), vec2(2,-2), vec2(-3,.5), vec2(3,.5), vec2(-2,-2), vec2(0,2)];
+    drawLineList(starPath, .2, MAGENTA, true, vec2(6, 0));
 
     // rects and lines
-    drawRect(vec2(0,-5), vec2(4,3), hsl(.6,1,.5), time);
-    drawLine(vec2(-5,-7), vec2(5,-3), 1,  hsl(0,0,1, .5));
-    drawLine(vec2(-5,-3), vec2(5,-7), .2, hsl(1,1,.5));
+    drawRect(vec2(0,-5), vec2(4,3), ORANGE, time);
+    drawLine(vec2(-5,-7), vec2(5,-3), 1,  hsl(0,0,1,.5));
+    drawLineList([vec2(5,-3), vec2(-5,-3), vec2(5,-7), vec2(-5,-7)], .2, GREEN);
 }
