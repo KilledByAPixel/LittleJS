7c730699ff8600250ff8f92aa489dc79eeb1b7f8
Frank Force <frankforce@gmail.com>
2025-10-08 21:18:26 -0500

    LittleJS Engine instead of Hello World

--- examples/shorts/helloWorld.js
+++ examples/shorts/helloWorld.js
@@ -4,7 +4,7 @@ function gameRender()
     drawRectGradient(cameraPos, getCameraSize(), BLACK, WHITE);
 
     // draw text to the overlay canvas
-    drawTextOverlay('Hello World', vec2(0,3), 3);
+    drawTextOverlay('LittleJS Engine', vec2(0,3), 3);
         
     // draw a tile
     drawTile(vec2(Math.sin(time)*3,-2), vec2(7), tile(3,128));
