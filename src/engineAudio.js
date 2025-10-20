a17cfbfdbb6224812e99e86e8c9f4d1925ff54d4
Frank Force <frankforce@gmail.com>
2025-10-20 01:22:32 -0500

    Add safety checks to getDuration methods
    Updated getDuration in Sound and SoundInstance classes to handle cases where sampleRate or rate may be undefined, returning 0 instead of causing errors.

--- src/engineAudio.js
+++ src/engineAudio.js
@@ -168,7 +168,7 @@ class Sound
      *  @return {number} - How long the sound is in seconds (undefined if loading)
      */
     getDuration()
-    { return this.sampleChannels && this.sampleChannels[0].length / this.sampleRate; }
+    { return this.sampleChannels && this.sampleRate && this.sampleChannels ?[0].length / this.sampleRate : 0; }
 
     /** Check if sound is loaded, for sounds fetched from a url
      *  @return {boolean} - True if sound is loaded and ready to play
@@ -427,7 +427,7 @@ class SoundInstance
     /** Get the total duration of this sound
      *  @return {number} - Total duration in seconds
      */
-    getDuration() { return this.sound.getDuration() / this.rate; }
+    getDuration() { return this.rate ? this.sound.getDuration() / this.rate : 0; }
 
     /** Get source of this sound instance
      *  @return {AudioBufferSourceNode}
