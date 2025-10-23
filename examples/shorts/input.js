83393f90c9a03708bc1f61196a4e73486175ad79
Frank Force <frankforce@gmail.com>
2025-10-22 23:47:10 -0500

    improve input demo

--- examples/shorts/input.js
+++ examples/shorts/input.js
@@ -1,6 +1,14 @@
 function gameUpdate()
 {
-    if (isUsingGamepad)
+    if (isTouchDevice)
+    {
+        debugText('Touch Gamepad Mode', vec2(0,4));
+        touchGamepadEnable = 1;
+
+        // touch input routed to mouse
+        debugPoint(mousePos, mouseIsDown(0) ? RED : YELLOW, 1);
+    }
+    else if (isUsingGamepad)
     {
         debugText('Gamepad Mode', vec2(0,4));
 
@@ -8,54 +16,45 @@ function gameUpdate()
         for (let i=2; i--;)
         {
             const stick = gamepadStick(i);
-            const pos = vec2(i?2:-2, 1);
-            debugCircle(pos, 2, WHITE);
-            debugLine(pos, pos.add(stick), GREEN);
+            const pos = vec2(i?3:-3, 1);
+            debugCircle(pos, 4, WHITE);
+            debugLine(pos, pos.add(stick.scale(2)), GREEN);
         }
 
         // buttons
         for (let i=16; i--;)
         {
-            const pos = vec2(-1.5 + i%4, -2 - (i/4|0));
+            const pos = vec2(-7 + i%8*2, -3 - (i/8|0)*2);
             if (gamepadIsDown(i))
-                debugCircle(pos, 1, RED, 0, 1);
-            debugCircle(pos, 1, WHITE);
-            debugText(i, pos, .5);
+                debugCircle(pos, 2, RED, 0, 1);
+            debugCircle(pos, 2, WHITE);
+            debugText(i, pos);
         }
-    }
-    else if (isTouchDevice)
-    {
-        debugText('Touch Mode', vec2(0,4));
-
-        // touch input routed to mouse
-        if (mouseIsDown(0))
-            debugCircle(mousePos, 4, RED, 0, 1);
-        debugCircle(mousePos, 4, WHITE);
     }
     else
     {
         debugText('Mouse and Keyboard Mode', vec2(0,4));
 
         // keyboard key (space bar)
-        debugRect(vec2(), vec2(2), WHITE);
+        debugRect(vec2(), vec2(4), WHITE);
         if (keyIsDown('Space'))
-            debugRect(vec2(), vec2(2), RED, 0, 0, 1);
+            debugRect(vec2(), vec2(4), RED, 0, 0, 1);
 
         // keyboard direction (arrow keys or WASD)
         const inputDirection = keyDirection();
-        debugLine(vec2(), inputDirection, GREEN);
+        debugLine(vec2(), inputDirection.scale(2), GREEN);
 
         // mouse pos
-        debugPoint(mousePos, mouseIsDown(0) ? RED : YELLOW);
+        debugPoint(mousePos, mouseIsDown(0) ? RED : YELLOW, 1);
 
         // mouse buttons
         for (let i=3; i--;)
         {
-            const pos = vec2(-1 + i, -3);
+            const pos = vec2(-2 + i*2, -4);
             if (mouseIsDown(i))
-                debugCircle(pos, 1, RED, 0, 1);
-            debugCircle(pos, 1, WHITE);
-            debugText(i, pos, .5);
+                debugCircle(pos, 2, RED, 0, 1);
+            debugCircle(pos, 2, WHITE);
+            debugText(i, pos);
         }
     }
 }
