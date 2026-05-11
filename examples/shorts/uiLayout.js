afa3ca67f1219c5a449c24c2d3df154d593197cb
Frank Force <frankforce@gmail.com>
2026-05-11 16:08:00 -0500

    refactor(ui): remove transparency option from outer layout initialization

--- examples/shorts/uiLayout.js
+++ examples/shorts/uiLayout.js
@@ -8,12 +8,12 @@ function gameInit()
     uiSystem.defaultShadowColor = BLACK;
     canvasClearColor = hsl(0,0,.3);
 
-    // outer vertical layout: title on top, grid below (transparent - just for positioning)
-    const menu = new UILayout(mainCanvasSize.scale(.5), 1, 20, 30, true);
+    // outer vertical layout: title on top, grid below
+    const menu = new UILayout(mainCanvasSize.scale(.5), 1, 20, 30);
     menu.addChild(new UIText(vec2(), vec2(400, 60), 'Level Select'));
 
     // inner 3x2 grid of level select buttons
-    // build it fully before adding to outer so outer reads its final size
+    // build fully before adding so outer reads its final size
     const grid = new UILayout(vec2(), 3, 20, 0, true);
     for (let i = 1; i <= 6; ++i)
     {
