056033103331bf91087798e0f8b34efede49366c
Frank Force <frankforce@gmail.com>
2026-05-14 14:44:20 -0500

    Bump engine version to 1.18.11.1 in both release and engine files

--- dist/littlejs.js
+++ dist/littlejs.js
@@ -35,7 +35,7 @@ const engineName = 'LittleJS';
  *  @type {string}
  *  @default
  *  @memberof Engine */
-const engineVersion = '1.18.11';
+const engineVersion = '1.18.11.1';
 
 /** Frames per second to update
  *  @type {number}
