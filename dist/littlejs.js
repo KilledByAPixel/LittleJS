ef7426cf68fadf0b3cb5c43bc80403d5fa6afae4
Frank Force <frankforce@gmail.com>
2026-05-26 15:48:31 -0500

    build

--- dist/littlejs.js
+++ dist/littlejs.js
@@ -35,7 +35,7 @@ const engineName = 'LittleJS';
  *  @type {string}
  *  @default
  *  @memberof Engine */
-const engineVersion = '1.18.15';
+const engineVersion = '1.18.15.1';
 
 /** Frames per second to update
  *  @type {number}
