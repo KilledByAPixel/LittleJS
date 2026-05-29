64635c9c524547c08ab0aeb9efc0f9ce7c37b034
Frank Force <frankforce@gmail.com>
2026-05-29 18:18:26 -0500

    improve demo

--- examples/shorts/crescent.js
+++ examples/shorts/crescent.js
@@ -6,12 +6,13 @@ function gameRender()
     {
         const percent = i/(count-1);        // 0=new .. .5=full .. 1=new
         const x = lerp(-8, 8, percent);
-        drawCrescent(vec2(x, 4), .9, percent, YELLOW, 0, false, .05, hsl(0,0,0,.5));
+        drawCrescent(vec2(x, 4), .9, percent, YELLOW, PI/2, false, .05, hsl(0,0,0,.5));
         drawText(percent.toFixed(2), vec2(x, 2.7), .4, WHITE);
     }
 
-    // a large moon animating continuously through its phases
+    // a large moon animating through its phases
     const percent = mod(time/4, 1);
-    drawCrescent(vec2(0,-3), 4, percent, hsl(.12,.5,.8), time/8, false, .15, hsl(.6,.4,.3));
+    drawCrescent(vec2(0,-3), 4, percent, hsl(.12,.5,.1), time/4, true, .15, hsl(.6,.4,.3));
+    drawCrescent(vec2(0,-3), 4, percent, hsl(.12,.5,.9), time/4, false, .15, hsl(.6,.4,.3));
     drawText('percent: ' + percent.toFixed(2), vec2(0,-8.7), .7, WHITE);
 }
