4d69a5e136911e235fa8948b91398acd2183c427
Frank Force <frankforce@gmail.com>
2026-06-01 13:52:07 -0500

    1.18.17

--- src/engine.js
+++ src/engine.js
@@ -32,7 +32,7 @@ const engineName = 'LittleJS';
  *  @type {string}
  *  @default
  *  @memberof Engine */
-const engineVersion = '1.18.16.1';
+const engineVersion = '1.18.17';
 
 /** Frames per second to update
  *  @type {number}
