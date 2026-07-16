b693dfa02504ed524cae4cd97c9fa52128ace6f5
Frank Force <frankforce@gmail.com>
2026-07-15 23:37:39 -0500

    smaller

--- examples/shorts/skydreams.js
+++ examples/shorts/skydreams.js
@@ -1,105 +1,87 @@
-cameraInFront = 3;
-focalLength = .7;
-shipPos = vec2();
-shipVelY = cameraZ = trackGap = 0;
-trackStartX = trackWidth = 3;
-trackRows = [];
+const cameraDistance = 3, focalLength = .7;
+let playerPos = vec2(), playerYSpeed = 0, playerZ = 0
+let trackX = 0, trackWidth = 3, trackGap = 0;
+let trackRows = [];
 
 function project(px, py, dz)
 {
-    const scale = getCameraSize().y * focalLength / dz;
-    const sway = (dz - cameraInFront)**2 / 50 * cos((cameraZ + dz) / 49);
-    return [vec2(
-        (px - shipPos.x + sway) * scale,
-        (py - 2 + dz*dz/50) * scale), scale];
-}
-
-function generateTrack(maxRow)
-{
-    for (let i = trackRows.length; i <= maxRow; )
-    {
-        if (trackGap < -8 && rand() < min(.2, i/1e4))
-            trackGap = min(6, 2 + i/400);
-        if (rand() < .1)
-        {
-            trackWidth = randInt(2,5);
-            trackStartX = max(0, min(7 - trackWidth, trackStartX + randInt(-2,3)));
-        }
-        trackGap--;
-
-        const row = trackRows[i++] = [];
-        for (let j = 7; j--;)
-            row[j] = i < 40 || trackGap < 0 
-            && rand() > clamp(cameraZ/1e4, .2, .5);
-    }
+    const s = getCameraSize().y * focalLength / dz;
+    const lift = dz**2 / 50;
+    const sway = (dz - cameraDistance)**2 * cos((playerZ + dz) / 50) / 50;
+    return vec2((px - playerPos.x + sway) * s, (py - 2 + lift) * s);
 }
 
 function gameUpdate()
 {
-    if (shipPos.y < -4)
+    if (playerPos.y < -4)
         return;
+    
+    // create more track if needed
+    for (let i = trackRows.length; i <= playerZ + 50; )
+    {
+        if (trackGap < -8 && rand() < .1)
+            trackGap = randInt(2, 5);
+        if (rand() < .1)
+        {
+            trackWidth = randInt(2, 5);
+            trackX = randInt(0, 7-trackWidth);
+        }
+        trackGap--;
 
-    generateTrack(cameraZ + 42 | 0);
+        // fill in the row with track data
+        let row = trackRows[i++] = [];
+        for (let j = 7; j--;)
+            row[j] = i < 40 || trackGap < 0
+                && trackX <= j && j < trackX + trackWidth;
+    }
 
-    // steer with arrow/WASD keys or mouse
-    shipPos.x += keyDirection().x*.1 || (mousePosScreen.x/mainCanvasSize.x - .5)/2;
+    // player control and physics
+    playerPos.x += keyDirection().x * .1;
+    playerPos.y += playerYSpeed -= .006;
+    playerZ += min(.5, .2 + playerZ/5e3);
 
-    // gravity and forward speed ramp
-    shipVelY -= .006;
-    shipPos.y += shipVelY;
-    cameraZ += min(.5, .2 + cameraZ/5e3);
-
-    // land and hop while jump is held
-    const jump = mouseIsDown(0) || keyIsDown('Space') ? .1 : 0;
-    const landRow = trackRows[cameraZ + cameraInFront | 0];
-    if (shipPos.y < 0 && shipPos.y > -.3 && landRow && landRow[round(shipPos.x + 3)])
-        shipPos.y = shipVelY = jump;
+    // land and jump
+    if (trackRows[playerZ + cameraDistance | 0][round(playerPos.x + 3)])
+    if (playerPos.y < 0 && playerPos.y > -.3)
+        playerPos.y = playerYSpeed = keyIsDown('Space') && .1;
 }
 
-function gameRenderPost()
+function gameRender()
 {
-    // sky gradient
+    // background sky gradient
     drawRectGradient(vec2(), getCameraSize(),
-        hsl((170 + cameraZ)/360, .7, .7), hsl((225 + cameraZ)/360, .7, .2));
+        hsl(.5, .8, .7), hsl(.7, .8, .2));
 
-    // track from far to near
-    for (r = cameraZ + 40 | 0; r > cameraZ; r--)
+    // draw track from far to near
+    for (let r = playerZ + 40 | 0; r > playerZ; r--)
+    for (let i = 7; i--;)
     {
-        row = trackRows[r];
-        if (!row)
-            continue;
-
-        for (i = 7; i--;)
-        {
-            if (!row[i])
-                continue;
-
-            dz = r - cameraZ;
-            [a] = project(i - 3.5, 0, dz);
-            [b] = project(i - 2.5, 0, dz);
-            [e] = project(i - 3.5, 0, dz + 1);
-            [f] = project(i - 2.5, 0, dz + 1);
-            hue = (99 + (r >> 7) * 70) / 360;
-            checker = r + i & 1;
-            height = 40 - r + cameraZ;
-
-            // front face
-            drawPoly([a, b, f, e], hsl(hue, .7, .6 + checker*.5));
-        }
+        const dz = r - playerZ;
+        const p = [
+            project(i - 3.5, 0, dz),
+            project(i - 2.5, 0, dz),
+            project(i - 2.5, 0, dz + 1),
+            project(i - 3.5, 0, dz + 1)];
+        const c = hsl(.3, .6, r+i&1?.4:.9);
+        if (trackRows[r][i])
+           drawPoly(p, c);
     }
 
     // draw player shadow
-    playerTrack = trackRows[cameraZ + cameraInFront | 0][round(shipPos.x + 3)];
-    if (shipPos.y >= 0 && playerTrack)
+    const s = getCameraSize().y * focalLength / cameraDistance;
+    if (playerPos.y >= 0)
+    if (trackRows[playerZ + cameraDistance | 0][round(playerPos.x + 3)])
     {
-        [p, s] = project(shipPos.x, 0, cameraInFront);
-        drawEllipse(p, vec2(s/2, s*2/9), hsl(0, 0, 0, .5));
+        const p = project(playerPos.x, 0, cameraDistance);
+        const c = hsl(0, 0, 0, .5);
+        drawEllipse(p, vec2(s/2, s/4), c);
     }
 
     // draw player
-    for (i = 99; i--;)
+    const p = project(playerPos.x, playerPos.y + .25, cameraDistance);
+    for (let i = 99; i--;)
     {
-        [p, s] = project(shipPos.x + .1 - i/1e3, shipPos.y + .35 - i/1e3, cameraInFront);
-        drawCircle(p, i*s/150, hsl((cameraZ/9 - i)/360, .9, .99 - i*.007));
+        const c = hsl(0, .9, 1 - i/150);
+        drawCircle(p.add(vec2(-i*s/1e3)), i*s/150, c);
     }
 }
