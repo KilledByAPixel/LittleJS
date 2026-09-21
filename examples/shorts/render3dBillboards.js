dfdefc3f70fc3de8261bb3e741592e7996ceb403
Frank Force <frankforce@gmail.com>
2026-09-21 13:20:50 -0500

    clean

--- examples/shorts/render3dBillboards.js
+++ examples/shorts/render3dBillboards.js
@@ -6,8 +6,8 @@ class Sprite extends EngineObject3D
         super(pos, undefined, tileInfo, color);
         this.size3D = vec3(2);
         this.softShadow = 2;
-        this.pixelated = true; // hard edged pixels, so tiles do not blur or bleed
         this.phase = rand(2*PI);
+        this.pixelated = true; // hard edged pixels
     }
     update()
     {
@@ -23,10 +23,11 @@ function gameInit()
     render3D.setSky(hsl(.65,.5,.15), hsl(.8,.4,.3), hsl(.65,.4,.1));
     render3D.setFog(15, 40);
     render3D.ambientColor = hsl(.6,.2,.5);
+    new CameraControl3D(vec3(), 15, .5, .003);
 
-    // the floor and cube for comparison
+    // floor and cube for comparison
     new EngineObject3D(vec3(), buildGrid(vec2(30), 1, hsl(.8,.2,.3)));
-    new EngineObject3D(vec3(0,1,0), buildBox(2).setColor(hsl(0,0,.7)));
+    new EngineObject3D(vec3(0,2,0), buildBox(2).setColor(hsl(0,0,.7)));
 
     // ring of sprites from the tile sheet
     for (let i = 12; i--;)
@@ -35,6 +36,4 @@ function gameInit()
         const color = hsl(i/12,.8,.7);
         new Sprite(pos, tile(i%4, 16), color);
     }
-
-    new CameraControl3D(vec3(), 15, .5, .003); // drag to turn, wheel to zoom
 }
--- examples/shorts/render3dShapes.js
+++ examples/shorts/render3dShapes.js
@@ -34,11 +34,8 @@ function buildShapes()
 function gameInit()
 {
     new Render3DPlugin;
-    render3D.setSky(hsl(.6,.8,.55), hsl(.6,1,.9));
-    render3D.setFog(15, 40);
+    render3D.setSky();
     render3D.shadows = true;
-    render3D.lightDirection = vec3(-.5,-1,-.3);
-    render3D.ambientColor = hsl(.6,.1,.38);
     new CameraControl3D(vec3(0,1,0), 15, .4, .003);
 
     // checkerboard floor
