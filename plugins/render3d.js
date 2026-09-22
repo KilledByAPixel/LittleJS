b31c5fd2d5b23e96857a0fae36ca338a5a88a9d0
Frank Force <frankforce@gmail.com>
2026-09-22 00:00:47 -0500

    build

--- plugins/render3d.js
+++ plugins/render3d.js
@@ -348,7 +348,7 @@ class Render3DPlugin
         // lights and fog
         /** @property {Vector3} - Direction the directional light travels, read at each draw;
          *  any length will do, both the shading and the shadows normalize it themselves */
-        this.lightDirection = vec3(.5, -1, .3);
+        this.lightDirection = vec3(.5, -1, -.3);
         /** @property {Color} - Directional light color */
         this.lightColor = WHITE.copy();
         /** @property {Color} - Ambient light color */
