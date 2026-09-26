b14b2814b02e8557b6734d82cd214f994f1c31de
Frank Force <frankforce@gmail.com>
2026-09-25 22:41:09 -0500

    add lava brick

--- examples/shorts/lightShadows.js
+++ examples/shorts/lightShadows.js
@@ -32,17 +32,21 @@ function gameInit()
     lightSystem.shadows = true;
 
     // a room with shadow casting pillars
-    for (let i = 0; i < 6; ++i)
+    for (let i=6; i--;)
         new Wall(vec2(-10 + i*4, 0), vec2(1, 3));
     new Wall(vec2(0, 8), vec2(24, 1));
     new Wall(vec2(0, -8), vec2(24, 1));
     new Glass(vec2(-2, 4), vec2(.5, 3), hsl(0, 1, .5));
     new Glass(vec2(4, 4), vec2(.5, 3), hsl(.6, 1, .5));
-    new Wall(vec2(-4, -5), vec2(2), tile(3), GREEN);
+    new Wall(vec2(-5, -5), vec2(2), tile(3), GREEN);
 
     // make a coin that does not cast a shadow
-    const coin = new EngineObject(vec2(4, -5), vec2(1), tile(0), 0, YELLOW);
+    const coin = new EngineObject(vec2(5, -5), vec2(1), tile(0), 0, YELLOW);
     coin.castShadow = false;
+
+    // make an emissive lava brick
+    const lava = new EngineObject(vec2(0, -5), vec2(1), tile(1), 0, RED);
+    lava.emissive = true;
 
     // make some shadow casting lights
     new Light(vec2(-7, 4), 8, hsl(.1,.8,.9));
